10636eb8e5f8a1ae0bf3b51b0cf0ea21c4616009
trending-wize-backend <Saif.albaqami@outlook.com>
2025-06-04 13:08:11 -0400

    Update income_statement.py

--- components/income_statement.py
+++ components/income_statement.py
@@ -3,9 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np # Import numpy for np.nan if you prefer that over None
 import plotly.express as px
-from utils.utils_helpers import get_neo4j_driver
+from utils.utils_helpers import *
 from utils.user_auth import (                   # ← pull everything from utils.py instead
-    format_value,
     calculate_delta,
     _arrow,
     R_display_metric_card,
